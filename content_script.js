4ca8dc2856dce88c8506e270ed98b409b453f4f2
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-08-11 01:18:46 +0300

    Optimize
    Better max zoom + avoid zooming over viewport

--- content_script.js
+++ content_script.js
@@ -1,10 +1,12 @@
 const zoom_by_default = true;   // Zoom in automatically when clicking a thumb in Discord
-const zoom_factor = 1.75;       // Enlargement (1.5 = 150%, etc.)
-const max_width_to_zoom = 512;  // Disable zoom if image width is larger than this (to avoid auto-zooming already large images)
-const max_height_to_zoom = 512; // Disable zoom if image height is larger than this (to avoid auto-zooming already large images)
+const max_zoom_factor = 2;      // Maximum zoom relative to image size (1.5 = 150%, 2 = 200% etc). Applies if viewport is larger than the zoomed modal.
+const max_zoom = 0.85;          // Maximum zoom relative to viewport (0.5 = 50%, 1 = 100% etc). Applies if the zoomed modal is larger than viewport.
+const max_width_to_zoom = 720;  // Disable zoom if image width is larger than this (to avoid auto-zooming already large images)
+const max_height_to_zoom = 720; // Disable zoom if image height is larger than this (to avoid auto-zooming already large images)
 
-var zoomer, modal;
-zoom = () => {
+var modal, zoomer, actual_zoom_factor, resizer;
+
+let zoom = () => {
   if (modal.classList.contains('xtZoomed')) {
     modal.classList.remove('xtZoomed');
     modal.style.cursor = 'zoom-in';
@@ -14,28 +16,56 @@ zoom = () => {
     modal.classList.add('xtZoomed');
     modal.style.cursor = 'zoom-out';
     modal.style.transition = '.25s';
-    modal.style.transform = 'scale('+zoom_factor+')';
+    modal.style.transform = 'scale('+actual_zoom_factor+')';
   }
 }
 
-document.addEventListener('readystatechange', (event) => {
-  if (document.readyState == 'complete'){
-    let container = document.querySelectorAll('.layerContainer-2v_Sit')[0];
-    if (container) {
-      container.addEventListener('DOMSubtreeModified', () => {
-        modal = container.querySelectorAll('[class^="modal-"]')[0];
-        if (modal) {
-          let img = modal.querySelectorAll('img')[0];
-          if (img && parseInt(img.style.width) <= max_width_to_zoom && parseInt(img.style.height) <= max_height_to_zoom) {
+let initZoomer = () => {
+
+  let container = document.querySelectorAll('.layerContainer-2v_Sit')[0];
+  if (container) {
+    container.addEventListener('DOMSubtreeModified', () => {
+      modal = container.querySelectorAll('[class^="modal-"]')[0];
+      if (modal) {
+
+        let img = modal.querySelectorAll('img')[0];
+        actual_zoom_factor = max_zoom_factor;
+
+        if (img) {
+          let iw = parseInt(img.style.width);
+          let ih = parseInt(img.style.height);
+        
+          if (iw * actual_zoom_factor > window.innerWidth * max_zoom || ih * actual_zoom_factor > window.innerHeight * max_zoom) {
+            let longest_img_side = iw > ih ? iw : ih;
+            let shortest_win_side = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
+            actual_zoom_factor = (shortest_win_side / longest_img_side) * max_zoom;
+          }
+
+          if (iw <= max_width_to_zoom && ih <= max_height_to_zoom) {
             img.addEventListener('click', () => { zoom(); });
             if (zoom_by_default) {
               zoomer = setTimeout(() => { 
                 if (container.innerHTML != '') zoom(); 
-              }, 400);
+              }, 500);
             }
           }
+
         }
-      });
-    }
+      }
+    });
+  }
+
+  document.addEventListener('resize', () => {
+    clearTimeout(resizer);
+    resizer = setTimeout(() => {
+      initZoomer();
+    }, 300);
+  });
+
+}
+
+document.addEventListener('readystatechange', (event) => {
+  if (document.readyState == 'complete'){
+    initZoomer();
   }
 });
