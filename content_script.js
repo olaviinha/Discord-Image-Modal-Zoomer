547688f22d47e1d9fca13218553bbe43bec8c015
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-12-05 11:11:35 +0200

    Fixes

--- content_script.js
+++ content_script.js
@@ -1,10 +1,10 @@
-const zoom_by_default = true;   // Zoom in automatically when clicking a thumb in Discord
-const max_zoom_factor = 2;      // Maximum zoom relative to image size (1.5 = 150%, 2 = 200% etc). Applies if viewport is larger than the zoomed modal.
-const max_zoom = 0.85;          // Maximum zoom relative to viewport (0.5 = 50%, 1 = 100% etc). Applies if the zoomed modal is larger than viewport.
-const max_width_to_zoom = 720;  // Don't auto-zoom if image width is larger than this (to avoid auto-zooming already large images)
-const max_height_to_zoom = 720; // Don't auto-zoom if image height is larger than this (to avoid auto-zooming already large images)
+const zoom_by_default = true;    // Zoom in automatically when clicking a thumb in Discord
+const max_zoom_factor = 200;     // Maximum zoom relative to image size in percentage (%).
+const max_zoom = 90;             // Maximum zoom relative to viewport in percentage (%).
+const max_width_to_zoom = 1400;  // Don't auto-zoom if image width is larger than this (to avoid auto-zooming already large images)
+const max_height_to_zoom = 1400; // Don't auto-zoom if image height is larger than this (to avoid auto-zooming already large images)
 
-var modal, zoomer, actual_zoom_factor, resizer;
+let modal, zoomer, actual_zoom_factor, resizer;
 
 const zoom = () => {
   if (modal.classList.contains('xtZoomed')) {
@@ -29,18 +29,17 @@ const initZoomer = () => {
       if (modal) {
 
         let img = modal.querySelectorAll('img')[0];
-        actual_zoom_factor = max_zoom_factor;
+        actual_zoom_factor = max_zoom_factor / 100;
 
         if (img) {
           let iw = parseInt(img.style.width);
           let ih = parseInt(img.style.height);
-        
-          if (iw * actual_zoom_factor > window.innerWidth * max_zoom || ih * actual_zoom_factor > window.innerHeight * max_zoom) {
+          if (iw * actual_zoom_factor > window.innerWidth * (max_zoom/100) || ih * actual_zoom_factor > window.innerHeight * (max_zoom/100)) {
             let longest_img_side = iw > ih ? iw : ih;
+            let shortest_img_side = iw < ih ? iw : ih;
             let shortest_win_side = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
-            actual_zoom_factor = (shortest_win_side / longest_img_side) * max_zoom;
+            actual_zoom_factor = (shortest_win_side / (window.innerWidth > window.innerHeight && iw > ih ? shortest_img_side : longest_img_side)) * (max_zoom/100);
           }
-
           img.addEventListener('click', () => { zoom(); });
           if (zoom_by_default && iw <= max_width_to_zoom && ih <= max_height_to_zoom) {
             zoomer = setTimeout(() => { 
